740f2215326516e298e7d066753593ed92cc8205
zinebsmine <57711712+zinebsmine@users.noreply.github.com>
2019-11-22 12:50:23 +0100

    Add files via upload

---
+++ source/myProtein.cpp
@@ -0,0 +1,68 @@
+#include "myProtein.h" 
+
+
+//rajouter taille chaine de caractères
+
+myProtein::myProtein()
+{
+	//header = " " ;
+	header = output_header();
+	sequence = output_sequence();
+}
+	
+
+	 // Getters
+	 
+	
+	string myProtein::getSequence() const{return sequence;}
+	
+	string myProtein::getHeader() const{return header;}
+	 
+
+// Function definition of output_header() to extract header from file Data Base 
+
+string myProtein::output_header() 
+{ 
+   // Object to read from file 
+    ifstream in("proteine.fasta");
+    
+    string line, name;
+	while(getline(in, line)) 
+		{ 
+		
+		if(line[0] == '>'){ // Identifier marker
+          name=line;  
+          
+          //std::cout << "name:" <<name << std::endl; 
+                
+          break;
+		} 
+	}
+	
+	in.close();
+  
+  return name;
+} 
+
+// Function definition of output_sequence() to extract sequence from file Data Base 
+
+string myProtein::output_sequence() 
+{ 
+   // Object to read from file 
+    ifstream in("proteine.fasta"); 
+
+    
+    string content,line;
+	while(getline(in, line)) 
+		{ 
+		
+		if(line[0] != '>'){ // Identifier marker
+			content=content+line;  
+				
+			//std::cout << " content:" <<content  << std::endl;    
+          
+		} 
+	}
+	
+  return content;
+}
---
+++ source/myProtein.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class myProtein {
+	
+	public:
+	
+	myProtein();
+	
+	
+	 // Getters
+	 
+	string getSequence() const;
+	string getHeader() const;
+	
+	private:
+
+	
+	// Instance variables 
+	string header;
+	string sequence;
+   
+
+	// Function declaration of output_header() to extract header from file Data Base 
+	string output_header();
+	
+	// Function declaration of output_sequence() to extract sequence from file Data Base 
+	string output_sequence();
+	
+		
+};
