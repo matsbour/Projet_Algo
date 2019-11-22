024e71b362daaf3ccefc5f2ab32b0c8e1d823dbe
neji601 <alexandremissenard@gmail.com>
2019-11-22 12:43:01 +0100

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,5 @@
 #include "handle_database.h"
 #include <cstring>
-#include "myProtein.h"
 
 //Main prend un argument le chemin vers la database
 
@@ -11,6 +10,7 @@ void display_vector(const vector<char>* subject)
 		if(i%80==0){cout << endl;}
 		cout << subject->at(i) ;
 	}
+	
 	cout << endl;
 }
 
@@ -18,28 +18,36 @@ int main(int argc, char* argv[])
 {
 	Handle_Database* database = new Handle_Database(argv[1]);
 	
-	cout << endl << "From fetch : "<< endl ;
-	database->fetch_prot_header(150);
-	database->fetch_prot_sequence(150);
-	display_vector(database->get_prot_header_active());
-	display_vector(database->get_prot_active());
+	string prot_sequence_query = "RRPARSGGDGGAPMTTGSRVVKYYDGSRRGSRRGLSTSGRSVKKDPAGLRDSLLSEDDRSAAAAPPPPPVHPVRDQLSSQLVRPSRGLGAYRTMSVFGSGWRPCRAAASHVRGAR";
+	unsigned int length = prot_sequence_query.size();
+	bool is_different = false ;
 	
+	
+	for(unsigned int i =0; i<database->get_database_size();++i)
+	{
+		if(database->get_size_sequence_prot(i) == prot_sequence_query.size())
+		{
+			is_different = false ;
+			database->fetch_prot_sequence(i); // on met a jour prot_active afin de contenir la i eme
+			for(unsigned int j=0; j<length; ++j)
+			{
+				if(database->get_prot_active()->at(j) != prot_sequence_query[j])
+				{
+					is_different = true ;
+					break;
+				}
+			}
+			
+			if(!is_different)
+			{
+				cout << "Trouve index : " << i << endl ;
+				break ;
+			}
+		}
+	}
 	
 	delete database ;
 	return 0 ;
 }
 
-
-int main() 
-{ 
-    // Creating object of the class 
-    myProtein object ; 
-  
-    // Extracting the header 
-   cout << object.header << endl;
-   cout << object.sequence << endl;
-
-  
-    return 0; 
-}
 
