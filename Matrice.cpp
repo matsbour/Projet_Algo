5c1c9f59a551235398082fe86be1753cc0fc7a49
matsbour <49977869+matsbour@users.noreply.github.com>
2019-12-06 12:58:14 +0100

    Update Matrice.cpp

--- Matrice.cpp
+++ Matrice.cpp
@@ -1,5 +1,9 @@
 map<char,int> blos_dictionnary;
 blos_dictionnary= { {'A',0}, {'R',1}, {'N',2}, {'D',3}, {'C',4}, {'Q',5}, {'E',6}, {'G',7}, {'H',8}, {'I',9}, {'L',10}, {'K',11}, {'M',12}, {'F',13}, {'P',14}, {'S',15}, {'T',16}, {'W',17}, {'Y',18}, {'V',19}, {'B',20}, {'Z',21}, {'X',22},{'*',23}};
+vector<vector<int>>* matri= new vector<vector<int>>();
+matri->resize(24);
+for (int i = 0; i<24; i++)
+	matri->at(i).resize(24);
 
 int matrice(string Res1, string Res2, const map<char,int>* blos_dictionnary, const vector<vector<int>>* matri){
 	i =0;
